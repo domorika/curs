fafbd04b1007ae796d711afed519df9d1fa3c96f
domorika <textx20@mail.ru>
2026-04-23 14:06:03 +0300

    пр1 done

---
+++ практы_курсак/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace практы_курсак.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ практы_курсак/Controllers/ServicesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoStudio.Models;
+
+namespace PhotoStudio.Controllers;
+
+public class ServicesController : Controller
+{
+    private List<Service> GetServices()
+    {
+        return new List<Service>
+        {
+            new() { Id = 1, Name = "Семейная фотосессия", Price = 5000, DurationMinutes = 60 },
+            new() { Id = 2, Name = "Портретная съёмка", Price = 4000, DurationMinutes = 45 },
+            new() { Id = 3, Name = "Ретушь фото", Price = 1500, DurationMinutes = 30 },
+            new() { Id = 4, Name = "Фотокнига", Price = 3500, DurationMinutes = 0 }
+        };
+    }
+
+    public IActionResult Index(string sortBy = "Name")
+    {
+        var services = GetServices();
+
+        services = sortBy switch
+        {
+            "Price" => services.OrderBy(s => s.Price).ToList(),
+            "Duration" => services.OrderBy(s => s.DurationMinutes).ToList(),
+            _ => services.OrderBy(s => s.Name).ToList()
+        };
+
+        ViewBag.SortBy = sortBy;
+        return View(services);
+    }
+}
